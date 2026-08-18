14bdeee2355ef0a249dfba19c279ab3ae18cef40
Ashwin Sanjay Dange <ashwindange543@gmail.com>
2026-08-18 02:47:20 -0700

    Delete FBS/Core Python/Assignments/Assignment 2-Basic directory

--- FBS/Core Python/Assignments/Assignment 2-Basic/TotalSalaryofEmployee.py
+++
@@ -1,9 +0,0 @@
-basic = float(input("Enter Basic Salary: "))
-
-da = basic * 10 / 100
-ta = basic * 12 / 100
-hra = basic * 15 / 100
-
-total = basic + da + ta + hra
-
-print("Total Salary =", total)
