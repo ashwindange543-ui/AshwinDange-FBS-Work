6e503330ec24ed730898b070a44bd3420d128fa9
Ashwin Sanjay Dange <ashwindange543@gmail.com>
2026-08-18 03:01:12 -0700

    Delete FBS/Core Python/Assignments/Assignment 3-Branching directory

--- FBS/Core Python/Assignments/Assignment 3-Branching/marriage_eligible.py
+++
@@ -1,9 +0,0 @@
-gender = input("Enter Gender (male/female): ")
-age = int(input("Enter Age: "))
-
-if(gender == "male" and age >= 21):
-    print("Eligible for Marriage")
-elif(gender == "female" and age >= 18):
-    print("Eligible for Marriage")
-else:
-    print("Not Eligible")
--- FBS/Core Python/Assignments/Assignment 3-Branching/profit_loss.py
+++
@@ -1,9 +0,0 @@
-cp = float(input("Enter Cost Price: "))
-sp = float(input("Enter Selling Price: "))
-
-if(sp > cp):
-    print("Profit =", sp - cp)
-elif(cp > sp):
-    print("Loss =", cp - sp)
-else:
-    print("No Profit No Loss")
