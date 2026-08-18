fb0a02ffe106596c27686c58f8437d59c091ac43
Ashwin Sanjay Dange <ashwindange543@gmail.com>
2026-08-18 02:46:21 -0700

    Add files via upload

---
+++ FBS/Core Python/Assignments/Assignment 1-Basic/que12.py
@@ -0,0 +1,9 @@
+import math
+
+# r = 3
+
+r =int(input("Enter the Radius (r):"))
+
+Volume = 4/3 * math.pi * r**3
+
+print("Volume of Sphere is:",Volume)
---
+++ FBS/Core Python/Assignments/Assignment 1-Basic/que2.py
@@ -0,0 +1,7 @@
+# length=50
+# breadth=60
+# Area = length * breadth
+l =int(input("Entar length 1: "))
+b =int(input("Entar breadth 2: "))
+Area = l * b
+print("Area of Rectangle is:",Area)
