2c7e4642c6438d8b523d3967f7cbcbabfc1cf1c4
Ashwin Sanjay Dange <ashwindange543@gmail.com>
2026-09-08 00:57:49 -0700

    Delete FBS/Core Python/Tests directory

--- FBS/Core Python/Tests/Test 1/que3.py
+++
@@ -1,6 +0,0 @@
-km = float(input("Enter distance in kilometers:"))
-
-meters = km * 1000
-centimeters = km * 100000
-print("Distance in meters:", meters)
-print("Distance in centimeters:", centimeters)
